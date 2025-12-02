20c835670a227095f896e4a0db01f691de4db204
Jason Steelman <js81535@uga.edu>
2025-12-02 15:16:39 -0500

    flesh out the processor

--- log_file_processor.r
+++ log_file_processor.r
@@ -3,4 +3,14 @@
 
 logged_data <- read.csv('binary_focal_crossentropy_without_core_with_batch_size_32.csv')
 
-print(head(logged_data))
+# print(head(logged_data))
+
+
+plot(logged_data$epoch, 
+     logged_data$val_loss, 
+     type = 'l', 
+     main = 'Validation Loss',
+     xlab = 'Epoch', 
+     ylab = 'Validation Loss')
+
+
