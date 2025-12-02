e5336544d93f25b5b914e40be6d1c999207df823
Jason Steelman <js81535@uga.edu>
2025-12-02 15:12:52 -0500

    make an R file that will process log csv files into visualizations

---
+++ log_file_processor.r
@@ -0,0 +1,6 @@
+## Log File Processor
+## This script turns model training log files into graphs for analysis
+
+logged_data <- read.csv('binary_focal_crossentropy_without_core_with_batch_size_32.csv')
+
+print(head(logged_data))
